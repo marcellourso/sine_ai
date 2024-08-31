e6f1f6c46efe6639ee9538a62a7062610f6f800b
urm37 <urm37@inwind.it>
2024-08-31 11:51:47 +0200

    Modifiche a main.c e app_x-cube-ai.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,387 +1,50 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "app_x-cube-ai.h"
 
 /* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
 #include <stdio.h>
 
-#include "ai_datatypes.h"
-#include "ai_platform.h"
-#include "sine_model.h"
-#include "sine_model_data.h"
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
 /* Private variables ---------------------------------------------------------*/
-UART_HandleTypeDef huart2;
+UART_HandleTypeDef huart2;  // Terminale di uscita
 UART_HandleTypeDef huart3;
-
-/* USER CODE BEGIN PV */
-GPIO_PinState button_state;
-AI_ALIGNED(4) ai_u8 in_data[AI_SINE_MODEL_IN_1_SIZE_BYTES];
-AI_ALIGNED(4) ai_u8 out_data[AI_SINE_MODEL_OUT_1_SIZE_BYTES];
-/* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART3_UART_Init(void);
 static void MX_USART2_UART_Init(void);
-/* USER CODE BEGIN PFP */
 
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
-
-  /* USER CODE BEGIN 1 */
-  char buf[50];
-  int buf_len=0;
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  /* MCU Configuration */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART3_UART_Init();
-  MX_USART2_UART_Init();
-  MX_X_CUBE_AI_Init();
+  MX_USART2_UART_Init();  // Inizializza huart2
+  MX_X_CUBE_AI_Init();   // Inizializza il modello AI
 
+  char buf[50];
+  int buf_len = sprintf(buf, "\r\n\r\nSTM32 X-Cube-AI test\r\n");
+  HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, 100);  // Usa huart2
 
-  /* USER CODE BEGIN 2 */
-  buf_len = sprintf(buf, "\r\n\r\nSTM32 X-Cube-AI test\r\n");
-  HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, 100);
-
-  // attivazioni intermedie
-  ai_u8 activations[AI_SINE_MODEL_DATA_ACTIVATIONS_SIZE];
-  ai_u8 weithtsNew[AI_SINE_MODEL_DATA_WEIGHTS_SIZE];
-
-  /* AI buffer IO handlers */
-    ai_buffer *ai_input;
-    ai_buffer *ai_output;
-    // handler del network la rete neurale sine_model
-    ai_handle network = AI_HANDLE_NULL;
-
-    ai_error err;
-    ai_network_report report;
-
-
-    /** @brief Initialize network */
-          const ai_handle acts[] = { activations };
-
-          err = ai_sine_model_create_and_init(&network, acts, NULL);
-          if (err.type != AI_ERROR_NONE) {
-    		  buf_len = sprintf(buf, "Error: could not create NN instance\r\n");
-    		  HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, 100);
-              //return -1;
-          }
-          else
-          {
-    		  buf_len = sprintf(buf, "success in create NN instance\r\n");
-    		  HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, 100);
-          }
-
-
-          if (ai_sine_model_get_report(network, &report) != true) {
-        	  buf_len = sprintf(buf, "ai get report error\r\n");
-        	  HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, 100);
-          }
-
-          // prepara l'inferenza riferiamoci al report che contiene tutti i parametri
-          ai_input = &report.inputs[0];
-          ai_output = &report.outputs[0];
-
-          //link inputs and outputs to the model
-          ai_input[0].data = AI_HANDLE_PTR(in_data);
-          ai_output[0].data = AI_HANDLE_PTR(out_data);
-
-
-          // stabiliamo il numero di batch, di solito 1
-          ai_i32 nbatch;
-
-          float x = 1.5;
-
-          for (uint32_t i = 0; i < AI_SINE_MODEL_IN_1_SIZE; i++)
-          			  {
-          				((ai_float *)in_data)[i] = (ai_float)x;
-          			  }
-
-          nbatch = ai_sine_model_run(network, &ai_input[0], &ai_output[0]);
-
-          if (nbatch != 1) {
-          			  buf_len = sprintf(buf, "Error: could not run inference\r\n");
-          			  HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, 100);
-          			}
-
-          float y_val=0;
-          y_val = ((float *)out_data)[0];
-
-          buf_len = sprintf(buf, "%f \r\n",y_val);
-
-          HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, 100);
-
-
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-
-  MX_X_CUBE_AI_Process();
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
-}
-
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 180;
-  RCC_OscInitStruct.PLL.PLLP = 2;
-  RCC_OscInitStruct.PLL.PLLQ = 2;
-  RCC_OscInitStruct.PLL.PLLR = 2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Activate the Over-Drive mode
-  */
-  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-  {
-    Error_Handler();
+    MX_X_CUBE_AI_Process();  // Processa l'inferenza AI
   }
 }
 
-/**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART2_UART_Init(void)
-{
+/* Configurazione del clock, UART e GPIO qui come nel tuo codice originale */
 
-  /* USER CODE BEGIN USART2_Init 0 */
-
-  /* USER CODE END USART2_Init 0 */
-
-  /* USER CODE BEGIN USART2_Init 1 */
-
-  /* USER CODE END USART2_Init 1 */
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
-  huart2.Init.WordLength = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART2_Init 2 */
-
-  /* USER CODE END USART2_Init 2 */
-
-}
-
-/**
-  * @brief USART3 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART3_UART_Init(void)
-{
-
-  /* USER CODE BEGIN USART3_Init 0 */
-
-  /* USER CODE END USART3_Init 0 */
-
-  /* USER CODE BEGIN USART3_Init 1 */
-
-  /* USER CODE END USART3_Init 1 */
-  huart3.Instance = USART3;
-  huart3.Init.BaudRate = 115200;
-  huart3.Init.WordLength = UART_WORDLENGTH_8B;
-  huart3.Init.StopBits = UART_STOPBITS_1;
-  huart3.Init.Parity = UART_PARITY_NONE;
-  huart3.Init.Mode = UART_MODE_TX_RX;
-  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART3_Init 2 */
-
-  /* USER CODE END USART3_Init 2 */
-
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : PC13 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
-
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
-}
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+  while (1) {}
 }
 
 #ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+  // Implementazione per la gestione degli errori
 }
 #endif /* USE_FULL_ASSERT */
--- X-CUBE-AI/App/app_x-cube-ai.c
+++ X-CUBE-AI/App/app_x-cube-ai.c
@@ -1,121 +1,38 @@
-
-/**
-  ******************************************************************************
-  * @file    app_x-cube-ai.c
-  * @author  X-CUBE-AI C code generator
-  * @brief   AI program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-
- /*
-  * Description
-  *   v1.0 - Minimum template to show how to use the Embedded Client API
-  *          model. Only one input and one output is supported. All
-  *          memory resources are allocated statically (AI_NETWORK_XX, defines
-  *          are used).
-  *          Re-target of the printf function is out-of-scope.
-  *   v2.0 - add multiple IO and/or multiple heap support
-  *
-  *   For more information, see the embeded documentation:
-  *
-  *       [1] %X_CUBE_AI_DIR%/Documentation/index.html
-  *
-  *   X_CUBE_AI_DIR indicates the location where the X-CUBE-AI pack is installed
-  *   typical : C:\Users\<user_name>\STM32Cube\Repository\STMicroelectronics\X-CUBE-AI\7.1.0
-  */
-
-#ifdef __cplusplus
- extern "C" {
-#endif
-
-/* Includes ------------------------------------------------------------------*/
-
-#if defined ( __ICCARM__ )
-#elif defined ( __CC_ARM ) || ( __GNUC__ )
-#endif
-
-/* System headers */
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <inttypes.h>
-#include <string.h>
-
 #include "app_x-cube-ai.h"
 #include "main.h"
 #include "ai_datatypes_defines.h"
 #include "sine_model.h"
 #include "sine_model_data.h"
 
-/* USER CODE BEGIN includes */
-/* USER CODE END includes */
-
-/* IO buffers ----------------------------------------------------------------*/
-
-#if !defined(AI_SINE_MODEL_INPUTS_IN_ACTIVATIONS)
+/* IO buffers */
 AI_ALIGNED(4) ai_i8 data_in_1[AI_SINE_MODEL_IN_1_SIZE_BYTES];
-ai_i8* data_ins[AI_SINE_MODEL_IN_NUM] = {
-data_in_1
-};
-#else
-ai_i8* data_ins[AI_SINE_MODEL_IN_NUM] = {
-NULL
-};
-#endif
+ai_i8* data_ins[AI_SINE_MODEL_IN_NUM] = { data_in_1 };
 
-#if !defined(AI_SINE_MODEL_OUTPUTS_IN_ACTIVATIONS)
 AI_ALIGNED(4) ai_i8 data_out_1[AI_SINE_MODEL_OUT_1_SIZE_BYTES];
-ai_i8* data_outs[AI_SINE_MODEL_OUT_NUM] = {
-data_out_1
-};
-#else
-ai_i8* data_outs[AI_SINE_MODEL_OUT_NUM] = {
-NULL
-};
-#endif
+ai_i8* data_outs[AI_SINE_MODEL_OUT_NUM] = { data_out_1 };
 
-/* Activations buffers -------------------------------------------------------*/
+/* Activations buffers */
+AI_ALIGNED(32) static uint8_t pool0[AI_SINE_MODEL_DATA_ACTIVATION_1_SIZE];
+ai_handle data_activations0[] = { pool0 };
 
-AI_ALIGNED(32)
-static uint8_t pool0[AI_SINE_MODEL_DATA_ACTIVATION_1_SIZE];
-
-ai_handle data_activations0[] = {pool0};
-
-/* AI objects ----------------------------------------------------------------*/
-
+/* AI objects */
 static ai_handle sine_model = AI_HANDLE_NULL;
-
 static ai_buffer* ai_input;
 static ai_buffer* ai_output;
 
+extern UART_HandleTypeDef huart2;  // Usa huart2
+
 static void ai_log_err(const ai_error err, const char *fct)
 {
-  /* USER CODE BEGIN log */
-  if (fct)
-    printf("TEMPLATE - Error (%s) - type=0x%02x code=0x%02x\r\n", fct,
-        err.type, err.code);
-  else
-    printf("TEMPLATE - Error - type=0x%02x code=0x%02x\r\n", err.type, err.code);
-
-  do {} while (1);
-  /* USER CODE END log */
+  char buf[100];
+  int buf_len = sprintf(buf, "Error (%s) - type=0x%02x code=0x%02x\r\n", fct, err.type, err.code);
+  HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, 100);  // Usa huart2 per la trasmissione
 }
 
 static int ai_boostrap(ai_handle *act_addr)
 {
   ai_error err;
 
-  /* Create and initialize an instance of the model */
   err = ai_sine_model_create_and_init(&sine_model, act_addr, NULL);
   if (err.type != AI_ERROR_NONE) {
     ai_log_err(err, "ai_sine_model_create_and_init");
@@ -125,31 +42,13 @@ static int ai_boostrap(ai_handle *act_addr)
   ai_input = ai_sine_model_inputs_get(sine_model, NULL);
   ai_output = ai_sine_model_outputs_get(sine_model, NULL);
 
-#if defined(AI_SINE_MODEL_INPUTS_IN_ACTIVATIONS)
-  /*  In the case where "--allocate-inputs" option is used, memory buffer can be
-   *  used from the activations buffer. This is not mandatory.
-   */
-  for (int idx=0; idx < AI_SINE_MODEL_IN_NUM; idx++) {
-	data_ins[idx] = ai_input[idx].data;
+  for (int idx = 0; idx < AI_SINE_MODEL_IN_NUM; idx++) {
+    ai_input[idx].data = data_ins[idx];
   }
-#else
-  for (int idx=0; idx < AI_SINE_MODEL_IN_NUM; idx++) {
-	  ai_input[idx].data = data_ins[idx];
-  }
-#endif
 
-#if defined(AI_SINE_MODEL_OUTPUTS_IN_ACTIVATIONS)
-  /*  In the case where "--allocate-outputs" option is used, memory buffer can be
-   *  used from the activations buffer. This is no mandatory.
-   */
-  for (int idx=0; idx < AI_SINE_MODEL_OUT_NUM; idx++) {
-	data_outs[idx] = ai_output[idx].data;
+  for (int idx = 0; idx < AI_SINE_MODEL_OUT_NUM; idx++) {
+    ai_output[idx].data = data_outs[idx];
   }
-#else
-  for (int idx=0; idx < AI_SINE_MODEL_OUT_NUM; idx++) {
-	ai_output[idx].data = data_outs[idx];
-  }
-#endif
 
   return 0;
 }
@@ -157,81 +56,55 @@ static int ai_boostrap(ai_handle *act_addr)
 static int ai_run(void)
 {
   ai_i32 batch;
-
   batch = ai_sine_model_run(sine_model, ai_input, ai_output);
   if (batch != 1) {
-    ai_log_err(ai_sine_model_get_error(sine_model),
-        "ai_sine_model_run");
+    ai_log_err(ai_sine_model_get_error(sine_model), "ai_sine_model_run");
     return -1;
   }
-
   return 0;
 }
 
-/* USER CODE BEGIN 2 */
 int acquire_and_process_data(ai_i8* data[])
 {
-  /* fill the inputs of the c-model
-  for (int idx=0; idx < AI_SINE_MODEL_IN_NUM; idx++ )
-  {
-      data[idx] = ....
-  }
+  static int angle = 0;
+  float radians = angle * (M_PI / 180.0);
+  ((ai_float*)data[0])[0] = radians;
 
-  */
+  angle = (angle + 1) % 360;
   return 0;
 }
 
 int post_process(ai_i8* data[])
 {
-  /* process the predictions
-  for (int idx=0; idx < AI_SINE_MODEL_OUT_NUM; idx++ )
-  {
-      data[idx] = ....
-  }
-
-  */
+  float y_val = ((ai_float*)data[0])[0];
+  char buf[50];
+  int buf_len = sprintf(buf, "Seno(%d gradi) = %f\r\n", angle, y_val);
+  HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, 100);  // Usa huart2
   return 0;
 }
-/* USER CODE END 2 */
-
-/* Entry points --------------------------------------------------------------*/
 
 void MX_X_CUBE_AI_Init(void)
 {
-    /* USER CODE BEGIN 5 */
-  printf("\r\nTEMPLATE - initialization\r\n");
-
+  printf("AI Model Initialization\r\n");
   ai_boostrap(data_activations0);
-    /* USER CODE END 5 */
 }
 
 void MX_X_CUBE_AI_Process(void)
 {
-    /* USER CODE BEGIN 6 */
   int res = -1;
 
-  printf("TEMPLATE - run - main loop\r\n");
-
   if (sine_model) {
-
     do {
-      /* 1 - acquire and pre-process input data */
-      res = acquire_and_process_data(data_ins);
-      /* 2 - process the data - call inference engine */
+      res = acquire_and_process_data(data_ins);  // Prepara i dati di input
       if (res == 0)
-        res = ai_run();
-      /* 3- post-process the predictions */
+        res = ai_run();  // Esegue l'inferenza
       if (res == 0)
-        res = post_process(data_outs);
-    } while (res==0);
+        res = post_process(data_outs);  // Elabora i risultati
+    } while (res == 0);
   }
 
   if (res) {
     ai_error err = {AI_ERROR_INVALID_STATE, AI_ERROR_CODE_NETWORK};
     ai_log_err(err, "Process has FAILED");
   }
-    /* USER CODE END 6 */
 }
-#ifdef __cplusplus
-}
-#endif
